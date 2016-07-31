c409abeec1e19d7db0ea38fbbc7904a75ee45c26
Alex Ball <aball6@mail.greenriver.edu>
2016-07-30 22:53:23 -0700

    Consolidated logic to functions. Harvesters now split between available sources in the starting room. Added if statement so that the console is not spammed by -6 errors when trying to spawn creeps every tick.

--- modules/main.js
+++ modules/main.js
@@ -1,5 +1,8 @@
 //Tell jshint (atom package) to stop showing certain irrelevent warnings.
 /*jshint esversion: 6 */
+
+var harvesterCapacity = 4;
+
 var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
@@ -10,6 +13,39 @@ var clearDeadCreepMemory = function() {
             delete Memory.creeps[name];
             console.log('Clearing non-existing creep memory:', name);
         }
+    }
+};
+
+var getNextSource = function() {
+    var harvestSource = 0;
+    var sources = Game.spawns.Spawn1.room.find(FIND_SOURCES);
+    if (Game.spawns.Spawn1.nextSource === undefined || Game.spawns.Spawn1.nextSource >= sources.length) {
+        Game.spawns.Spawn1.nextSource = 0;
+    }
+    else {
+        harvestSource = sources[Game.spawns.Spawn1.nextSource];
+        Game.spawns.Spawn1.nextSource += 1;
+    }
+    return harvestSource;
+};
+
+var spawnUpgrader = function() {
+    var harvestSource = getNextSource();
+
+    var creepMemory = {role: 'upgrader', source: harvestSource};
+    var newName = Game.spawns.Spawn1.createCreep([WORK,CARRY,MOVE], undefined, creepMemory);
+    if (newName != -6) {
+        console.log('Spawning new upgrader: ' + newName);
+    }
+};
+
+var spawnHarvester = function () {
+    var harvestSource = getNextSource();
+
+    var creepMemory = {role: 'harvester', source: harvestSource};
+    var newName = Game.spawns.Spawn1.createCreep([WORK,CARRY,MOVE], undefined, creepMemory);
+    if (newName != -6) {
+        console.log('Spawning new harvester: ' + newName);
     }
 };
 
@@ -35,9 +71,15 @@ module.exports.loop = function () {
     var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
     console.log('Harvesters: ' + harvesters.length);
 
-    if(harvesters.length < 4) {
-        var newName = Game.spawns.Spawn1.createCreep([WORK,CARRY,MOVE], undefined, {role: 'harvester'});
-        console.log('Spawning new harvester: ' + newName);
+    var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
+    console.log('Upgraders: ' + upgraders.length);
+
+    if(harvesters.length < harvesterCapacity) {
+        spawnHarvester();
+    }
+
+    if(upgraders.length < harvesterCapacity) {
+        spawnUpgrader();
     }
 
     for(var name in Game.creeps) {
