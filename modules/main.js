515f2b53771b3a72309bd41a627af7117d25c40b
Alex Ball <aball6@mail.greenriver.edu>
2016-07-30 22:27:38 -0700

    Added spawning harvesters logic.

--- modules/main.js
+++ modules/main.js
@@ -4,6 +4,15 @@ var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
 
+var clearDeadCreepMemory = function() {
+    for(var name in Memory.creeps) {
+        if(!Game.creeps[name]) {
+            delete Memory.creeps[name];
+            console.log('Clearing non-existing creep memory:', name);
+        }
+    }
+};
+
 module.exports.loop = function () {
 
     var tower = Game.getObjectById('519170edb0e79b2eeb360e71');
@@ -19,6 +28,16 @@ module.exports.loop = function () {
         if(closestHostile) {
             tower.attack(closestHostile);
         }
+    }
+
+    clearDeadCreepMemory();
+
+    var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+    console.log('Harvesters: ' + harvesters.length);
+
+    if(harvesters.length < 4) {
+        var newName = Game.spawns.Spawn1.createCreep([WORK,CARRY,MOVE], undefined, {role: 'harvester'});
+        console.log('Spawning new harvester: ' + newName);
     }
 
     for(var name in Game.creeps) {
