a0f91cec7797ef901d441528e1e3625fd83f9839
Alex Ball <aball6@mail.greenriver.edu>
2016-08-05 07:12:37 -0700

    Fixed issue with harvester logic where a big harvester would drop off at one structure then go gather energy again.

--- modules/role.harvester.js
+++ modules/role.harvester.js
@@ -11,15 +11,20 @@ var roleHarvester = {
    * @param {Creep} creep The creep that uses this role.
    */
   run: function(creep) {
-      if (creep.carry.energy < creep.carryCapacity) {
-        //If this harvester is not full of energy, go gather source.
+      if (creep.memory.working && creep.carry.energy === 0) {
+        //If the repairer is trying to return but has no energy,
+        //go gather more source.
+        creep.memory.working = false;
+        creep.say('harvesting');
+      }
+      if (!creep.memory.working && creep.carry.energy == creep.carryCapacity) {
+        //If the harvester is not returning but is full of energy,
+        //return.
+        creep.memory.working = true;
+        creep.say('returning energy');
+      }
 
-        //let sources = creep.room.find(FIND_SOURCES);
-        let closestSource = creep.pos.findClosestByPath(FIND_SOURCES);
-        if (creep.harvest(closestSource) == ERR_NOT_IN_RANGE) {
-          creep.moveTo(closestSource);
-        }
-      } else {
+      if (creep.memory.working) {
         //Find the closest structure that needs energy.
         let target = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
           filter: (structure) => {
@@ -35,6 +40,14 @@ var roleHarvester = {
           if (creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
             creep.moveTo(target);
           }
+        }
+      } else {
+        //If this harvester is not full of energy, go gather source.
+
+        //let sources = creep.room.find(FIND_SOURCES);
+        let closestSource = creep.pos.findClosestByPath(FIND_SOURCES);
+        if (creep.harvest(closestSource) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(closestSource);
         }
       }
     } //End run function.
