56e97f9111ee76a7ba27e5d76843c56380e8dfd0
Alex Ball <aball6@mail.greenriver.edu>
2017-04-20 22:04:24 -0700

    Copied over simple spawning logic from tutorial 4

--- main.js
+++ main.js
@@ -23,6 +23,24 @@ module.exports.loop = function() {
 
   FUNCTIONS.checkForLevelUp(spawn);
 
+  if (Object.keys(Memory.creepInfo.harvesters).length < 2) {
+    let newName = Game.spawns.Spawn1.createCreep([WORK, WORK, CARRY, MOVE], undefined, {
+      role: 'harvester'
+    });
+    console.log('Spawning new harvester: ' + newName);
+  }
+
+  if (Game.spawns.Spawn1.spawning) {
+    let spawningCreep = Game.creeps[Game.spawns.Spawn1.spawning.name];
+    Game.spawns.Spawn1.room.visual.text(
+      '🛠️' + spawningCreep.memory.role,
+      Game.spawns.Spawn1.pos.x + 1,
+      Game.spawns.Spawn1.pos.y, {
+        align: 'left',
+        opacity: 0.8
+      });
+  }
+
   for (let name in Game.creeps) {
     let creep = Game.creeps[name];
     if (creep.memory.role == 'harvester') {
