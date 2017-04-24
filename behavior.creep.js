042e05cf5c6b1e604b1706ab4d242d0453369d32
Alex Ball <aball6@mail.greenriver.edu>
2017-04-24 08:10:50 -0700

    Changed gatherFromClosestSource to literally look for the closestByRange active source. This fixes the bug where harvesters would try to go back and forth between sources instead of actually harvesting.

--- behavior.creep.js
+++ behavior.creep.js
@@ -1,11 +1,11 @@
 /**
-Gather from the nearest source.
+Gather from the nearest active source.
 @param {Creep} creep
 */
 function gatherFromClosestSource(creep) {
-  let sources = creep.room.find(FIND_SOURCES);
-  if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-    creep.moveTo(sources[0], {
+  let source = creep.pos.findClosestByRange(FIND_SOURCES_ACTIVE);
+  if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+    creep.moveTo(source, {
       visualizePathStyle: {
         stroke: '#ffaa00'
       }
