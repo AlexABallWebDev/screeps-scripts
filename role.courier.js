b9406f14e82a1a0b76f89c9889759f602235f118
Alex Ball <aball6@mail.greenriver.edu>
2017-04-23 18:34:06 -0700

    Adjusted creep logic to try to dump all held energy before continuing to gather energy. creeps are more vocal now as well.

--- role.courier.js
+++ role.courier.js
@@ -4,7 +4,17 @@ const roleCourier = {
 
   /** @param {Creep} creep **/
   run: function(creep) {
-    if (creep.carry.energy < creep.carryCapacity) {
+    if (creep.memory.carting && creep.carry.energy === 0) {
+      creep.memory.carting = false;
+      creep.say('💰 loot');
+    }
+
+    if (!creep.memory.carting && creep.carry.energy == creep.carryCapacity) {
+      creep.memory.carting = true;
+      creep.say('🔄 carting');
+    }
+
+    if (!creep.memory.carting) {
       creepBehavior.pickupBiggestEnergyPile(creep);
     } else {
       creepBehavior.dropOffEnergyAtNearbyStructure(creep);
--- role.harvester.js
+++ role.harvester.js
@@ -4,7 +4,17 @@ const roleHarvester = {
 
   /** @param {Creep} creep **/
   run: function(creep) {
-    if (creep.carry.energy < creep.carryCapacity) {
+    if (creep.memory.carting && creep.carry.energy === 0) {
+      creep.memory.carting = false;
+      creep.say('🌄 harvest');
+    }
+
+    if (!creep.memory.carting && creep.carry.energy == creep.carryCapacity) {
+      creep.memory.carting = true;
+      creep.say('🔄 carting');
+    }
+
+    if (!creep.memory.carting) {
       creepBehavior.gatherFromClosestSource(creep);
     } else {
       creepBehavior.dropOffEnergyAtNearbyStructure(creep);
