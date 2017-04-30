63d47ee52e5e4ba106bf53ac8e3c830f28a00323
Alex Ball <aball6@mail.greenriver.edu>
2017-04-30 16:03:22 -0700

    Added a for loop to define the spawn. Prevents an error where there are no spawns in a room and the room tries to create a creep.

--- main.js
+++ main.js
@@ -63,55 +63,57 @@ module.exports.loop = function() {
       }
     });
 
-    let spawn = spawns[0];
+    for (let spawnIndex in spawns) {
+      let spawn = spawns[spawnIndex];
 
-    const CREEP_BODY = {
-      defender: [ATTACK, ATTACK, MOVE, MOVE, ATTACK, ATTACK, MOVE, MOVE],
-      harvester: [WORK, MOVE, CARRY, MOVE],
-      courier: [CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE, CARRY, MOVE, CARRY,
-        MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY
-      ],
-      miner: [WORK, WORK, MOVE, WORK, WORK, MOVE, WORK],
-      builder: [WORK, WORK, CARRY, MOVE, MOVE, MOVE, CARRY, WORK, MOVE, CARRY,
-        WORK, MOVE, CARRY, WORK, MOVE, CARRY, WORK, MOVE, CARRY, WORK, MOVE, CARRY, WORK
-      ],
-      upgrader: [WORK, WORK, CARRY, MOVE, CARRY, WORK, MOVE, WORK, WORK,
-        MOVE, CARRY, WORK, MOVE, WORK, WORK, MOVE, CARRY, WORK, MOVE, WORK, WORK, MOVE
-      ]
-    };
+      const CREEP_BODY = {
+        defender: [ATTACK, ATTACK, MOVE, MOVE, ATTACK, ATTACK, MOVE, MOVE],
+        harvester: [WORK, MOVE, CARRY, MOVE],
+        courier: [CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE, CARRY, MOVE, CARRY,
+          MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY, MOVE, CARRY
+        ],
+        miner: [WORK, WORK, MOVE, WORK, WORK, MOVE, WORK],
+        builder: [WORK, WORK, CARRY, MOVE, MOVE, MOVE, CARRY, WORK, MOVE, CARRY,
+          WORK, MOVE, CARRY, WORK, MOVE, CARRY, WORK, MOVE, CARRY, WORK, MOVE, CARRY, WORK
+        ],
+        upgrader: [WORK, WORK, CARRY, MOVE, CARRY, WORK, MOVE, WORK, WORK,
+          MOVE, CARRY, WORK, MOVE, WORK, WORK, MOVE, CARRY, WORK, MOVE, WORK, WORK, MOVE
+        ]
+      };
 
-    roomFunctions.checkForSources(room);
-    let sourceIdMissingMiner = roomFunctions.findSourceIdMissingMiner(room);
+      roomFunctions.checkForSources(room);
+      let sourceIdMissingMiner = roomFunctions.findSourceIdMissingMiner(room);
 
-    if (_.size(room.find(FIND_HOSTILE_CREEPS)) > 0 && _.size(creepsOfRole.defender) < 2) {
-      spawnFunctions.createCreepWithRole(spawn, "defender", CREEP_BODY.defender);
-    } else if (_.size(Game.creeps) < 2) {
-      spawnFunctions.createCreepWithRole(spawn, "harvester", CREEP_BODY.harvester);
-    } else if (_.size(creepsOfRole.courier) < 1) {
-      spawnFunctions.createCreepWithRole(spawn, "courier", CREEP_BODY.courier);
-    } else if (sourceIdMissingMiner) {
-      let minerName = roomFunctions.buildMiner(room, sourceIdMissingMiner, spawn, CREEP_BODY.miner);
-      if (minerName == ERR_NOT_ENOUGH_ENERGY && _.size(creepsOfRole.miner) < 1 && _.size(creepsOfRole.harvester) < 2) {
+      if (_.size(room.find(FIND_HOSTILE_CREEPS)) > 0 && _.size(creepsOfRole.defender) < 2) {
+        spawnFunctions.createCreepWithRole(spawn, "defender", CREEP_BODY.defender);
+      } else if (_.size(Game.creeps) < 2) {
         spawnFunctions.createCreepWithRole(spawn, "harvester", CREEP_BODY.harvester);
+      } else if (_.size(creepsOfRole.courier) < 1) {
+        spawnFunctions.createCreepWithRole(spawn, "courier", CREEP_BODY.courier);
+      } else if (sourceIdMissingMiner) {
+        let minerName = roomFunctions.buildMiner(room, sourceIdMissingMiner, spawn, CREEP_BODY.miner);
+        if (minerName == ERR_NOT_ENOUGH_ENERGY && _.size(creepsOfRole.miner) < 1 && _.size(creepsOfRole.harvester) < 2) {
+          spawnFunctions.createCreepWithRole(spawn, "harvester", CREEP_BODY.harvester);
+        }
+      } else if (_.size(creepsOfRole.courier) < 3) {
+        spawnFunctions.createCreepWithRole(spawn, "courier", CREEP_BODY.courier);
+      } else if (_.size(room.find(FIND_MY_CONSTRUCTION_SITES)) > 0 && _.size(creepsOfRole.builder) < 2) {
+        spawnFunctions.createCreepWithRole(spawn, "builder", CREEP_BODY.builder);
+      } else if (_.size(creepsOfRole.upgrader) < 2) {
+        spawnFunctions.createCreepWithRole(spawn, "upgrader", CREEP_BODY.upgrader);
       }
-    } else if (_.size(creepsOfRole.courier) < 3) {
-      spawnFunctions.createCreepWithRole(spawn, "courier", CREEP_BODY.courier);
-    } else if (_.size(room.find(FIND_MY_CONSTRUCTION_SITES)) > 0 && _.size(creepsOfRole.builder) < 2) {
-      spawnFunctions.createCreepWithRole(spawn, "builder", CREEP_BODY.builder);
-    } else if (_.size(creepsOfRole.upgrader) < 2) {
-      spawnFunctions.createCreepWithRole(spawn, "upgrader", CREEP_BODY.upgrader);
+
+      spawnFunctions.displayCreateCreepVisual(spawn);
+
+      if (!Memory.flags[room.name + " upContainer"]) {
+        roomConstruction.placeUpgraderContainer(room, spawn.pos);
+      }
+
+      roomConstruction.addExtensionsToRoom(room, spawn.pos);
+
+      roomFunctions.createTowerAssignments(room);
+
+      roomConstruction.placeTowers(room, spawn.pos);
     }
-
-    spawnFunctions.displayCreateCreepVisual(spawn);
-
-    if (!Memory.flags[room.name + " upContainer"]) {
-      roomConstruction.placeUpgraderContainer(room, spawn.pos);
-    }
-
-    roomConstruction.addExtensionsToRoom(room, spawn.pos);
-
-    roomFunctions.createTowerAssignments(room);
-
-    roomConstruction.placeTowers(room, spawn.pos);
   }
 };
