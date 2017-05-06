faeb6cc5acde916e65c0afcca9e005d803348d78
Alex Ball <aball6@mail.greenriver.edu>
2017-05-06 13:35:41 -0700

    Refactored main script so that some per-room actions are only taken if the room is owned by me. Also moved creep role running and counting into the room loop so that each room can run independently of the others.

--- main.js
+++ main.js
@@ -39,65 +39,67 @@ module.exports.loop = function() {
 
   towerFunctions.runTowerLogic();
 
-  let creepsOfRole = {};
-
-  for (let roleName in roles) {
-    creepsOfRole[roleName] = {};
-  }
-
-  for (let name in Game.creeps) {
-    let creep = Game.creeps[name];
-    if (roles[creep.memory.role]) {
-      creepsOfRole[creep.memory.role][name] = creep.id;
-      roles[creep.memory.role].run(creep);
-    }
-  }
-
   for (let roomName in Game.rooms) {
     let room = Game.rooms[roomName];
 
-    utilityFunctions.checkForLevelUp(room);
+    roomFunctions.checkForSources(room);
 
-    let spawns = room.find(FIND_MY_STRUCTURES, {
-      filter: {
-        structureType: STRUCTURE_SPAWN
+    let creepsOfRole = {};
+
+    for (let roleName in roles) {
+      creepsOfRole[roleName] = {};
+    }
+
+    for (let name in Game.creeps) {
+      let creep = Game.creeps[name];
+      if (roles[creep.memory.role]) {
+        creepsOfRole[creep.memory.role][name] = creep.id;
+        roles[creep.memory.role].run(creep);
       }
-    });
+    }
 
-    for (let spawnIndex in spawns) {
-      let spawn = spawns[spawnIndex];
-
-      roomFunctions.checkForSources(room);
-      let sourceIdMissingMiner = roomFunctions.findSourceIdMissingMiner(room);
-
-      if (_.size(room.find(FIND_HOSTILE_CREEPS)) > 0 && _.size(creepsOfRole.defender) < 2) {
-        spawnFunctions.createCreepWithRole(spawn, "defender", creepBody.defender);
-      } else if (_.size(Game.creeps) < 2) {
-        spawnFunctions.createCreepWithRole(spawn, "harvester", creepBody.harvester);
-      } else if (_.size(creepsOfRole.courier) < 1) {
-        spawnFunctions.createCreepWithRole(spawn, "courier", creepBody.courier);
-      } else if (sourceIdMissingMiner) {
-        let minerName = roomFunctions.buildMiner(room, sourceIdMissingMiner, spawn, creepBody.miner);
-        if (minerName == ERR_NOT_ENOUGH_ENERGY && _.size(creepsOfRole.miner) < 1 && _.size(creepsOfRole.harvester) < 2) {
-          spawnFunctions.createCreepWithRole(spawn, "harvester", creepBody.harvester);
-        }
-      } else if (_.size(creepsOfRole.courier) < 3) {
-        spawnFunctions.createCreepWithRole(spawn, "courier", creepBody.courier);
-      } else if (_.size(room.find(FIND_MY_CONSTRUCTION_SITES)) > 0 && _.size(creepsOfRole.builder) < 2) {
-        spawnFunctions.createCreepWithRole(spawn, "builder", creepBody.builder);
-      } else if (_.size(creepsOfRole.upgrader) < 2) {
-        spawnFunctions.createCreepWithRole(spawn, "upgrader", creepBody.upgrader);
-      }
-
-      spawnFunctions.displayCreateCreepVisual(spawn);
-
-      roomConstruction.placeUpgraderContainer(room, spawn.pos);
-
-      roomConstruction.addExtensionsToRoom(room, spawn.pos);
+    if (room.controller && room.controller.my) {
+      utilityFunctions.checkForLevelUp(room);
 
       roomFunctions.createTowerAssignments(room);
 
-      roomConstruction.placeTowers(room, spawn.pos);
+      let spawns = room.find(FIND_MY_STRUCTURES, {
+        filter: {
+          structureType: STRUCTURE_SPAWN
+        }
+      });
+
+      for (let spawnIndex in spawns) {
+        let spawn = spawns[spawnIndex];
+        let sourceIdMissingMiner = roomFunctions.findSourceIdMissingMiner(room);
+
+        if (_.size(room.find(FIND_HOSTILE_CREEPS)) > 0 && _.size(creepsOfRole.defender) < 2) {
+          spawnFunctions.createCreepWithRole(spawn, "defender", creepBody.defender);
+        } else if (_.size(Game.creeps) < 2) {
+          spawnFunctions.createCreepWithRole(spawn, "harvester", creepBody.harvester);
+        } else if (_.size(creepsOfRole.courier) < 1) {
+          spawnFunctions.createCreepWithRole(spawn, "courier", creepBody.courier);
+        } else if (sourceIdMissingMiner) {
+          let minerName = roomFunctions.buildMiner(room, sourceIdMissingMiner, spawn, creepBody.miner);
+          if (minerName == ERR_NOT_ENOUGH_ENERGY && _.size(creepsOfRole.miner) < 1 && _.size(creepsOfRole.harvester) < 2) {
+            spawnFunctions.createCreepWithRole(spawn, "harvester", creepBody.harvester);
+          }
+        } else if (_.size(creepsOfRole.courier) < 3) {
+          spawnFunctions.createCreepWithRole(spawn, "courier", creepBody.courier);
+        } else if (_.size(room.find(FIND_MY_CONSTRUCTION_SITES)) > 0 && _.size(creepsOfRole.builder) < 2) {
+          spawnFunctions.createCreepWithRole(spawn, "builder", creepBody.builder);
+        } else if (_.size(creepsOfRole.upgrader) < 2) {
+          spawnFunctions.createCreepWithRole(spawn, "upgrader", creepBody.upgrader);
+        }
+
+        spawnFunctions.displayCreateCreepVisual(spawn);
+
+        roomConstruction.placeUpgraderContainer(room, spawn.pos);
+
+        roomConstruction.addExtensionsToRoom(room, spawn.pos);
+
+        roomConstruction.placeTowers(room, spawn.pos);
+      }
     }
   }
 };
