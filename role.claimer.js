f336f2c42d69aacf266f5dc95ab1a71426ed32f9
Alex Ball <alexball@alexball.me>
2018-05-20 18:23:07 -0700

    Rearranged code so that the creep only tries to claim the room if it is inside the room with the claim flag.

--- role.claimer.js
+++ role.claimer.js
@@ -5,21 +5,23 @@ let roleClaimer = {
     if (claimFlag) {
       // Move towards the controller (even if not in the room)
       // until in range to claim the controller.
-      let claimResult = creep.claimController(creep.room.controller);
       if (creep.room.name != claimFlag.pos.roomName) {
         creep.moveTo(claimFlag.pos, {
           visualizePathStyle: {
             stroke: '#B99CFB'
           }
         });
-      } else if (claimResult == ERR_NOT_IN_RANGE) {
-        creep.moveTo(creep.room.controller, {
-          visualizePathStyle: {
-            stroke: '#B99CFB'
-          }
-        });
       } else {
-        console.log(creep.name + ' cannot claim room due to error: ' + claimResult);
+        let claimResult = creep.claimController(creep.room.controller);
+        if (claimResult == ERR_NOT_IN_RANGE) {
+          creep.moveTo(creep.room.controller, {
+            visualizePathStyle: {
+              stroke: '#B99CFB'
+            }
+          });
+        } else {
+          console.log(creep.name + ' cannot claim room due to error: ' + claimResult);
+        }
       }
     } else {
       console.log('Claimer ' + creep.name + ' Cannot find a newClaim flag.');
