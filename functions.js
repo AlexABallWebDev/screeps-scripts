6311b03ee850128ca6ed43d635041807dabe3f56
Alex Ball <alexball@alexball.me>
2018-05-20 18:52:33 -0700

    Fixed colonistNames name in memory.

--- functions.js
+++ functions.js
@@ -59,7 +59,7 @@ function respawn() {
       Memory.flags = {};
       Memory.rooms = {};
       Memory.spawns = {};
-      Memory.colonists = {};
+      Memory.colonistNames = {};
 
       Memory.controllerSign = "ALL YOUR BASE ARE BELONG TO US.";
 
